fd41173701c6d736a51050dfe0f003416dc8e7ae
Mark <mark@thewebgenetix.com>
2025-05-27 10:40:43 +0200

    installation success

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    className=" flex-1  bg-black "
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className=" text-white ">Edit app/index.tsx to edit this screen screen.</Text>
     </View>
   );
 }
