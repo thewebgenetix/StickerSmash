e38a9c4ae87f27fc1a3fb1d7ba13cac032351c7a
Mark <mark@thewebgenetix.com>
2025-05-27 10:58:40 +0200

    check ok tailW

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-    className=" flex-1  bg-black "
+    className=" flex-1 justify-center bg-black "
     >
       <Text className=" text-white ">Edit app/index.tsx to edit this screen screen.</Text>
     </View>
