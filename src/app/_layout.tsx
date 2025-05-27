1045355f15c120e16d5df6cb6bfd8451efcc7a38
Mark <mark@thewebgenetix.com>
2025-05-27 16:34:29 +0200

    implemented theme

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,20 @@
 import "@/global.css";
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack>
-
-  </Stack>;
+  const myTheme = {
+    ...DarkTheme, 
+    colors: {
+      ...DarkTheme.colors,
+      primary: 'white',
+    },
+  };
+  return(
+  <ThemeProvider value = {myTheme}> 
+  <Stack>  
+  </Stack>
+  </ThemeProvider>
+ 
+  )
 }
