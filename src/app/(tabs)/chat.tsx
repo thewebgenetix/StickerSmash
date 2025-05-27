70cc2f4014e59a092260f500a5dd536a0402312c
Mark <mark@thewebgenetix.com>
2025-05-27 16:17:11 +0200

    preparation no theme done

--- src/app/(tabs)/chat.tsx
+++ src/app/(tabs)/chat.tsx
@@ -1,7 +1,9 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function ChatScreen() {
   return (
+    <View className='flex-1 justify-center items-center'> 
     <Text>Chat Screen</Text>
+    </View>
   )
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,7 @@
 import "@/global.css";
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack>
-
-  </Stack>;
+  return <Slot>
+  </Slot>
 }
